4cba053d8155caced551777b64bfa82c6cbb2b8b
James <jamesianburns@gmail.com>
2016-11-09 09:59:34 -0500

    Some changes to chess logic.

--- src/chess_logic.js
+++ src/chess_logic.js
@@ -10,14 +10,45 @@ export function getAvailableMoves(teamwhite, teamblack, piece, isWhite) {
   }
 }
 
-export function hasPiece(pieces, pos) {
+var toColInt = function( colString ) {
+  return colString.toLowerCase().codePointAt(0) - 'a'.codePointAt(0) + 1;
+}
+
+var toColString = function( colInt ) {
+  return String.fromCharCode( colInt - 1 + 'a'.codePointAt(0) );
+}
+
+
+var hasPiece = function(pieces, rowInt, colInt) {
   for (piece in pieces) {
-    if (pos === pieces[piece].position) {
+    var pieceRow = parseInt(pieces[piece].position[1]);
+    var pieceCol = toColInt(pieces[piece].position[0]);
+
+    if (pieceRow == rowInt && pieceCol == colInt) {
       return true;
     }
   }
   return false;
 }
+
+var hitPiece = function( rowInt, colInt, teamwhite, teamblack, isWhite, possibleMoves ) { 
+
+  // if blocked by team piece
+  if ( hasPiece( (isWhite ? teamwhite : teamblack), rowInt, colInt ) ) {
+    return true;
+  }
+  // if blocked by enemy piece
+  else if ( hasPiece( (isWhite ? teamwhite : teamblack), rowInt, colInt ) ) {
+    possibleMoves.push(toColString(colInt) + rowInt);
+    return true;
+  }
+  else {
+    possibleMoves.push(toColString(colInt) + rowInt);
+    return false;
+  }
+}
+
+
 
 
 export function getPawnMoves(teamwhite, teamblack, piece, isWhite) {
@@ -25,38 +56,33 @@ export function getPawnMoves(teamwhite, teamblack, piece, isWhite) {
   var possibleMoves = [];
 
   var forward = isWhite ? 1 : -1;
-  var col = piece.position[0];
-  var row = piece.position[1];
-  var rowInt = parseInt(row);
+  var colInt = toColInt(piece.position[0]);
+  var rowInt = parseInt(piece.position[1]);
 
   if (rowInt + forward > 0 && rowInt + forward < 9) {
 
     // one forward
-    var oneForward = col + (rowInt + forward);
-    if (!hasPiece(teamwhite, oneForward) && !hasPiece(teamblack, oneForward)) {
-      possibleMoves.push(oneForward);
+    if (!hasPiece(teamwhite, rowInt+forward, colInt) && !hasPiece(teamblack, rowInt+forward, colInt)) {
+      possibleMoves.push(toColString(colInt) + rowInt+forward);
 
       // two forward
       if ((isWhite ? rowInt == 2 : rowInt == 7)) {
-        var twoForward = col + (rowInt + 2*forward);
-        if (!hasPiece(teamwhite, twoForward) && !hasPiece(teamblack, twoForward)) {
-          possibleMoves.push(twoForward);
+        if (!hasPiece(teamwhite, rowInt+2*forward, colInt) && !hasPiece(teamblack, rowInt+2*forward, colInt)) {
+          possibleMoves.push(toColString(colInt) + rowInt+2*forward);
         }
       }
     }
 
     // forward-left
-    if (col > 'a') {
-      var forwardLeft = String.fromCharCode(col.codePointAt(0) - 1) + (rowInt + forward);
-      if ( hasPiece( (isWhite ? teamblack : teamwhite), forwardLeft)) {
-        possibleMoves.push(forwardLeft);
+    if (col > 'A') {
+      if ( hasPiece( (isWhite ? teamblack : teamwhite), rowInt+forward, colInt-1)) {
+        possibleMoves.push(toColString(colInt-1) + (rowInt+forward));
       }
     }
     // forward-right
-    if (col < 'h') {
-      var forwardRight = String.fromCharCode(col.codePointAt(0) + 1) + (rowInt + forward);
-      if ( hasPiece( (isWhite ? teamblack : teamwhite), forwardRight)) {
-        possibleMoves.push(forwardRight);
+    if (col < 'H') {
+      if ( hasPiece( (isWhite ? teamblack : teamwhite), rowInt+forward, colInt+1)) {
+        possibleMoves.push(toColString(colInt+1) + (rowInt+forward));
       }
     }
 
@@ -65,5 +91,37 @@ export function getPawnMoves(teamwhite, teamblack, piece, isWhite) {
 
 }
 
-export function testfunc() {
+export function getRookMoves(teamwhite, teamblack, piece, isWhite) {
+
+  var possibleMoves = [];
+  var colInt = toColInt(piece.position[0]);
+  var rowInt = parseInt(piece.position[1]);
+
+
+  // iterate up
+  for (var possibleRow = rowInt + 1; possibleRow <= 8; ++possibleRow) {
+    if (hitPiece( possibleRow, colInt, teamwhite, teamblack, isWhite, possibleMoves) ) {
+      break;
+    }
+  }
+  // iterate down
+  for (var possibleRow = rowInt - 1; possibleRow >= 1; --possibleRow) {
+    if (hitPiece( possibleRow, colInt, teamwhite, teamblack, isWhite, possibleMoves) ) {
+      break;
+    }
+  }
+  // iterate left
+  for (var possibleCol = colInt + 1; possibleCol <= 8; ++possibleCol) {
+    if (hitPiece( rowInt, possibleCol, teamwhite, teamblack, isWhite, possibleMoves) ) {
+      break;
+    }
+  }
+  // iterate right
+  for (var possibleCol = colInt - 1; possibleCol >= 1; --possibleCol) {
+    if (hitPiece( rowInt, possibleCol, teamwhite, teamblack, isWhite, possibleMoves) ) {
+      break;
+    }
+  }
+
 }
+
